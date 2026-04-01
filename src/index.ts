b8df81e383eec0162680b183b05c30333210df99
forgetmelodyXL <asd13852710118@gmail.com>
2026-04-01 10:07:05 +0800

    1.3.8

--- src/index.ts
+++ src/index.ts
@@ -134,9 +134,9 @@ export function apply(ctx: Context, config: Config) {
 
       // 翻译常见错误信息
       errorMessage = errorMessage.replace(/connect ECONNREFUSED/i, '服务器已关闭')
-      errorMessage = errorMessage.replace(/connect ETIMEDOUT/i, '服务器连接超时')
-      errorMessage = errorMessage.replace(/connect ENOTFOUND/i, 'DNS服务器配置错误')
-      errorMessage = errorMessage.replace(/getaddrinfo EAI_AGAIN/i, 'DNS服务器配置错误')
+      errorMessage = errorMessage.replace(/connect ETIMEDOUT/i, '网络波动，请稍后尝试')
+      errorMessage = errorMessage.replace(/connect ENOTFOUND/i, '网络波动，请稍后尝试')
+      errorMessage = errorMessage.replace(/getaddrinfo EAI_AGAIN/i, '网络波动，请稍后尝试')
       
       // 移除IP:端口和域名
       errorMessage = errorMessage.replace(/\s+(\d+\.\d+\.\d+\.\d+):\d+/, '')
